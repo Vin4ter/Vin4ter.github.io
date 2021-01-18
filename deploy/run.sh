9d2cc8dfea7f16354342f7a006d127a4ea4da71d
Vin4ter <65507989+Vin4ter@users.noreply.github.com>
2021-01-18 19:46:36 +0300

    Add files via upload

---
+++ deploy/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /srv/openstore-api/src/
+source /srv/openstore/env.sh
+node /srv/openstore-api/src/index.js
---
+++ env/attach_web.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker attach --sig-proxy=false openstorewebdev_web_1
